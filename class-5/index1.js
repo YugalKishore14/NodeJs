7e8d11ae422e4a8a6dc3f3f6d8a359b777cfeefb
yugaldhiman <yugaldhiman14@gmail.com>
2025-01-15 11:19:15 +0530

    add 2 functions

--- class-5/index1.js
+++ class-5/index1.js
@@ -2,5 +2,5 @@
 // console.log(math.hello1(100, 200), math.hello2(100, 200));
 
 
-let { hello1, hello2 } = require('./index2')
-console.log(hello1(100, 200), hello2(200, 100));
+let { hello1, hello2, hello3, hello4 } = require('./index2')
+console.log(hello1(100, 200), hello2(200, 100), hello3(20, 10), hello4(200, 10));
--- class-5/index2.js
+++ class-5/index2.js
@@ -4,4 +4,10 @@ function hello1(a, b) {
 function hello2(a, b) {
     return a - b;
 }
-module.exports = { hello1, hello2 };
+function hello3(a, b) {
+    return a * b;
+}
+function hello4(a, b) {
+    return a / b;
+}
+module.exports = { hello1, hello2, hello3, hello4 };
