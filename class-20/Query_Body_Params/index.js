dbacf1c89b952313cbb66a5982cb4bb069dab6e9
yugaldhiman <yugaldhiman14@gmail.com>
2025-03-05 00:22:29 +0530

    req.body , params , req.query , pug

---
+++ class-20/Query_Body_Params/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+app.get('/news', (req, res) => {
+    res.send([
+        { id: 1, message: "This is Article one" },
+        { id: 2, message: "This is Article Two" },
+        { id: 3, message: "This is Article Three" },
+        { id: 4, message: "This is Article Four" }
+    ]);
+})
+
+// params
+app.get('/post/:id', (req, res) => {
+    let dataId = req.params.id;
+    console.log("Post Id is" + dataId);
+    res.send(dataId)
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.send(
+        {
+            id: 1,
+            message: "login",
+            data: req.body,
+            query: req.query
+        })
+})
+
+app.listen(port, () => {
+    console.log(`Sever Is live http://localhost:${port}`);
+})
---
+++ class-20/moduleWrapper/ModuleWrapper.js
@@ -0,0 +1,9 @@
+(
+    function (exports, require, module, __filename, __dirname) {
+    }
+)
+console.log(exports);
+console.log(require);
+console.log(module);
+console.log(__filename);
+console.log(__dirname);
